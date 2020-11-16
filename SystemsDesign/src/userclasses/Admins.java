c90baf67f79851c00ce764e84d62ff70e73101de
FluffyCherub <73240956+FluffyCherub@users.noreply.github.com>
2020-11-16 14:37:36 +0000

    Update Admins.java

--- SystemsDesign/src/userclasses/Admins.java
+++ SystemsDesign/src/userclasses/Admins.java
@@ -1,2 +1,24 @@
 package userclasses;
 //Admin class
+/*
+What needs doing:
+
+Add users
+Remove users
+Change access permissions
+
+Create departments
+
+Add degree courses
+Remove degree courses
+Assign courses to a department (options for multiple departments)
+Indicate lead department
+
+Add modules.
+Remove Modules
+Link modules to degrees and level of study (setting core or not)
+
+Update the system after changes
+Display results
+
+*/

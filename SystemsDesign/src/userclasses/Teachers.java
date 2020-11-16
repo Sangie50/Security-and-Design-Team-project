85eedb9fff9b1112c5c602d05adac0e6d1039537
FluffyCherub <73240956+FluffyCherub@users.noreply.github.com>
2020-11-16 17:03:38 +0000

    testing how git works with netbeans (i just don't know)

--- SystemsDesign/src/userclasses/Teachers.java
+++ SystemsDesign/src/userclasses/Teachers.java
@@ -1,2 +1,33 @@
 package userclasses;
 //Teacher class
+/*
+Methods:
+Add Grades
+Update grades
+Options for resit grades
+calculate the weighted mean grade
+Decide whether student passes
+Based on the calculated grade, register student for next year
+Transition to next year
+Resit a year
+Graduate
+Drop out
+Take weighted average of all years to give a final score
+Calculate classification from final score
+View status of student
+Show outcome what student has achieved each period and level of study 
+Show outcome of whole degree
+Attribute 
+*/
+public class Teachers {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args){
+        // TODO code application logic here
+        System.out.println("Testing this crap 2: Electro Boogaloo");
+
+    }
+
+}

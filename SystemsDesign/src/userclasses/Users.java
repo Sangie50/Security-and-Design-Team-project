3089613f2b94fc1ad6c55b31a9f7ca589c5a43f3
wifimug <44526624+wifimug@users.noreply.github.com>
2020-11-18 18:15:51 +0000

    added more empty class files

--- SystemsDesign/src/userclasses/Users.java
+++ SystemsDesign/src/userclasses/Users.java
@@ -22,30 +22,6 @@ public abstract class Users {
 	public String getPassword() {
 		return password;
 	}
-	
-	public void addUser(String email, String name, String password) throws Exception {
-		Connection con = null;
-		try {
-			con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
-			Statement stmt = null;
-			try {
-				stmt = con.createStatement();
-				stmt.executeUpdate( "INSERT email = '" + email + "' surname = '" + surname + "' forename = '" + forename + "'");
-			}
-			catch (SQLException ex) {
-				ex.printStackTrace();
-			}
-			finally {
-				if (stmt != null) stmt.close();
-			}
-		}
-		catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		finally {
-			if (con != null) con.close();
-		}
-	}
 }
 	
 

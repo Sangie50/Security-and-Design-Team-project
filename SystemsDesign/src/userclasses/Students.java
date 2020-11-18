7474e1a20999f5ca8c001f35790ae9701342f6fa
Sangamithra Bala Amuthan <sbalaamuthan1@sheffield.ac.uk>
2020-11-18 18:17:27 +0000

    students class

--- SystemsDesign/src/userclasses/Students.java
+++ SystemsDesign/src/userclasses/Students.java
@@ -1,2 +1,41 @@
 package userclasses;
-//Student class
+import java.sql.*;
+import java.util.*;
+//Students class
+
+public class Students extends Users{
+  Integer periodOfStudy;
+  Integer startDate;
+  Integer endDate;
+  
+  public Integer getPeriodOfStudy() {
+  	return periodOfStudy;
+  }
+  
+  public Integer getstartDate() {
+  	return startDate;
+  }
+  
+  public Integer getEndDate() {
+  	return endDate;
+  }
+  
+  public static void main(String[] args)throws SQLException {
+      // TODO code application logic here
+      Connection con = null;  // a Connection object
+      try{
+          con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
+      } catch (Exception ex) {
+          ex.printStackTrace();
+      } 
+      Statement stmt = null;
+      try{
+          stmt = con.createStatement();
+      }catch (SQLException ex) {
+          ex.printStackTrace();
+      }finally {
+          if (stmt != null)
+              stmt.close();
+      }
+  }
+}

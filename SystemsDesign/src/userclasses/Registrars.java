1ba12cdb25923feb275ff2497eedbf8ba076dbcd
wifimug <44526624+wifimug@users.noreply.github.com>
2020-11-18 18:43:20 +0000

    tweaked permission dictionary

--- SystemsDesign/src/userclasses/Registrars.java
+++ SystemsDesign/src/userclasses/Registrars.java
@@ -1,5 +1,24 @@
 package userclasses;
 
 public class Registrars extends Users {
+	/*
+	What needs doing (methods):
+	[-]Add students
+	[-]Remove students
+	[-]Register initial period of study
+	[X]Link to degree
+	[X]
+	[-]Remove degree courses
+	[-]Assign courses to a department (options for multiple departments)
+	[-]Indicate lead department (part of degree)
+	[X]Add modules.
+	[-]Remove Modules
+	[-]Link modules to degrees and level of study (setting core or not)
+	[-]Update the system after changes
+	[-]Display results
+
+	Attributes to add
+	inherits from user class
+	*/
 	
 }

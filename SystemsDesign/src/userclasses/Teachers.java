2ac38f90a5450d6ab0ecac243a7332c520d92ddb
FluffyCherub <73240956+FluffyCherub@users.noreply.github.com>
2020-11-19 02:39:33 +0000

    Added addGrades, changeGrades, addResistGrade

--- SystemsDesign/src/userclasses/Teachers.java
+++ SystemsDesign/src/userclasses/Teachers.java
@@ -5,20 +5,17 @@ import java.util.*;
 
 /*
 Methods:
-[~]Add Grades
-[-]Update grades
-[-]Add options for resit grades
-[-]calculate the weighted mean grade
-[-]Decide whether student passes
-[?]Based on the calculated grade, register student for next year
-[?]Transition to next year
-[?]Resit a year
-[?]Graduate or Drop out
+[X]Add Grades
+[X]Update grades 
+[X]Resit grades
+[-]Calculate the weighted mean grade of the module
+[?]Decide whether student passes
+[?]Based on the calculated grade, register student for next year ,Transition to next year, Resit a year, Graduate or Drop out
 [?]Take weighted average of all years to give a final score
-[?]Calculate classification from final score
+[-]Calculate classification from the module
 [-]View status of student
-[-]Show outcome what student has achieved each period and level of study 
-[-]Show outcome of whole degree
+[GUI]Show outcome what student has achieved each period and level of study 
+[GUI]Show outcome of whole degree
 
 Unique Attributes: 
 Employee Number
@@ -35,14 +32,14 @@ public class Teachers extends Users{
         return departmentID;
     }
     
-    public void AddTeacher(String teacherEmail, String departmentID) throws SQLException {
+    public void addGrade(String moduleID, Integer registrationID, Integer overallMark) throws SQLException {
         Connection con = null;
         try {
             con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
             Statement stmt = null;
             try {
                 stmt = con.createStatement();
-                stmt.executeUpdate("INSERT INTO teacher (email, departmentID) VALUES('"+teacherEmail+"','"+departmentID+"')");
+                stmt.executeUpdate("INSERT INTO module grade VALUES ("+moduleID+","+registrationID+","+overallMark+","+null+")");
             }
             catch (SQLException ex) {
                 ex.printStackTrace();
@@ -59,14 +56,38 @@ public class Teachers extends Users{
         }
     }
     
-    public void AddGrades(String studentEmail, Integer grade) throws SQLException {
+    public void updateGrades(String moduleID, Integer registrationID, Integer overallMark) throws SQLException {
         Connection con = null;
         try {
             con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
             Statement stmt = null;
             try {
                 stmt = con.createStatement();
-                stmt.executeUpdate("UPDATE module_grade WHERE email = '" + studentEmail + "'SET grade? = '"+ grade +"'");
+                stmt.executeUpdate("UPDATE module grade SET overallMark = '"+overallMark+"' WHERE moduleID = '"+moduleID+"' AND registrationID = '"+registrationID+"'");
+            }
+            catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+            finally {
+                if (stmt != null) stmt.close();
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            if (con != null) con.close();
+        }
+    }
+    
+    public void addResitGrades(String moduleID, Integer registrationID, Integer resitMark) throws SQLException {
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
+            Statement stmt = null;
+            try {
+                stmt = con.createStatement();
+                stmt.executeUpdate("UPDATE module grade SET resitMark = '"+resitMark+"' WHERE moduleID = '"+moduleID+"' AND registrationID = '"+registrationID+"'");
             }
             catch (SQLException ex) {
                 ex.printStackTrace();

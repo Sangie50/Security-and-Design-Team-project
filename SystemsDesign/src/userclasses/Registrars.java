5b2b694b1a36edf55e0d600b3ac448f9abe0e9f5
wifimug <44526624+wifimug@users.noreply.github.com>
2020-11-19 00:20:19 +0000

    added methods to registrars

--- SystemsDesign/src/userclasses/Registrars.java
+++ SystemsDesign/src/userclasses/Registrars.java
@@ -12,16 +12,27 @@ public class Registrars extends Users {
 	[x]Remove students
 	[x]Register initial period of study
 	[?]Link to degree
-	[-]Add optional modules
-	[-]Remove optional modules
+	[x]Add optional modules
+	[x]Remove optional modules
 	[-]Check registration is complete
-	[-]Check student's modules
-	[-]Check credits of modules sum to 120
-	[-]Display results
+	[GUI]Check student's modules
+	[x]Check credits of modules sum to 120
+	[GUI]Display results
 
 	Attributes to add
 	inherits from user class
 	*/
+	
+	 // BUS : business school, COM : computer science, PSY : psychology, LAN : modern languages
+	 static Dictionary<String, String> degreeCode = new Hashtable<String, String>();
+	 
+	 static {
+		 degreeCode.put("Business School", "BUS");
+	     degreeCode.put("Computer Science", "COM");
+	     degreeCode.put("Psychology", "PSY");
+	     degreeCode.put("Modern Language", "LAN");
+	 }
+     
 	
 	/**
 	 * Assigns student or teacher to a user account
@@ -58,7 +69,6 @@ public class Registrars extends Users {
 	 
 	 public void deleteStudent(String email) throws SQLException {
 		 Connection con = null;
-	        //System.out.println(permission.get(newPermission));
 	        try {
 	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
 	            Statement stmt = null;
@@ -88,7 +98,6 @@ public class Registrars extends Users {
 	 
 	 public void registerInitialPeriodOfStudy(String email, int date) throws SQLException {
 		 Connection con = null;
-	        //System.out.println(permission.get(newPermission));
 	        try {
 	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
 	            Statement stmt = null;
@@ -116,19 +125,11 @@ public class Registrars extends Users {
 	        }
 	 }
 	 
-	 public void linkDegree(String email, String degree, int levelOfStudy, String undergradOrPostgrad, boolean yearInIndustry) throws SQLException {
+	 public void linkDegree(String email, String degree, int levelOfStudy, String undergradOrPostgrad, int serial) throws SQLException {
 		 Connection con = null;
-		// BUS : business school, COM : computer science, PSY : psychology, LAN : modern languages
-	        Dictionary<String, String> degreeCode = new Hashtable<String, String>();
-	        degreeCode.put("Business School", "BUS");
-	        degreeCode.put("Computer Science", "COM");
-	        degreeCode.put("Psychology", "PSY");
-	        degreeCode.put("Modern Language", "LAN");
 	        String degreeId = "";
-	        int serial = (int)(Math.random() * (99 - 10 + 1) + 10);
 	        
-	        if (yearInIndustry) degreeId = degreeCode.get(degree) + undergradOrPostgrad.toUpperCase() + serial;
-	        else degreeId = degreeCode.get(degree) + undergradOrPostgrad.toUpperCase() + serial;
+	        degreeId = degreeCode.get(degree) + undergradOrPostgrad.toUpperCase() + serial;
 	      
 	        try {
 	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
@@ -155,5 +156,112 @@ public class Registrars extends Users {
 	        finally {
 	            if (con != null) con.close();
 	        }
+	 }
+	 
+	 public void addModule(int serial, String name, int levelOfStudy, int credits, String department, int passMark, String undergradOrPostgrad) throws SQLException {
+		 Connection con = null; 
+		 String moduleId = degreeCode.get(department) + undergradOrPostgrad + serial;
+		 try {
+	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
+	            Statement stmt = null;
+	            try {
+	                stmt = con.createStatement();
+	                	stmt.executeQuery("INSERT INTO Modules VALUES moduleId = '" + moduleId + "' departmentId = '" + degreeCode.get(department) + "' moduleName = '" + name + "' levelOfStudy = '" + levelOfStudy + "' creditsWorth = '" + credits + "' passMark = " + passMark + "')"); 
+	                }
+
+	            catch (SQLException ex) {
+	                ex.printStackTrace();
+	            }
+	            finally {
+	                if (stmt != null) stmt.close();
+	            }
+	        }
+	        catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	        finally {
+	            if (con != null) con.close();
+	        }		 
+	 }
+	 
+	 public void deleteModule(String moduleId) throws SQLException {
+		 Connection con = null; 
+		 try {
+	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
+	            Statement stmt = null;
+	            try {
+	                stmt = con.createStatement();
+                	stmt.executeUpdate("DELETE FROM module WHERE moduleId = '"+ moduleId+"')");
+   
+	            }
+	            catch (SQLException ex) {
+	                ex.printStackTrace();
+	            }
+	            finally {
+	                if (stmt != null) stmt.close();
+	            }
+	        }
+	        catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	        finally {
+	            if (con != null) con.close();
+	        }		 		 
+	 }
+	 
+	 public void checkRegistration (String email) throws SQLException {
+		 Connection con = null; 
+		 try {
+	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
+	            Statement stmt = null;
+	            try {
+	                stmt = con.createStatement();
+                	stmt.executeUpdate("");
+   
+	            }
+	            catch (SQLException ex) {
+	                ex.printStackTrace();
+	            }
+	            finally {
+	                if (stmt != null) stmt.close();
+	            }
+	        }
+	        catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	        finally {
+	            if (con != null) con.close();
+	        }			 
+	 }
+	 
+	 public void checkModuleSum (int moduleId) throws SQLException{
+		 Connection con = null; 
+		 try {
+	            con = DriverManager.getConnection("jdbc:mysql://stusql.dcs.shef.ac.uk/team028", "team028", "714e454e");
+	            Statement stmt = null;
+	            try {
+	            	int totalCredits = 0;
+	                stmt = con.createStatement();
+                	ResultSet creditsList = stmt.executeQuery("SELECT creditsWorth FROM Module WHERE moduleId = '" + moduleId + "')");
+                	while (creditsList.next()) totalCredits += creditsList.getInt(1);
+                	if (totalCredits != 120) {
+                		System.err.println("Credits do not sum to 120. Invalid module selections.");
+                	}
+                	else System.out.println("Credits sum to 120.");
+	            }
+	            catch (SQLException ex) {
+	                ex.printStackTrace();
+	            }
+	            finally {
+	                if (stmt != null) stmt.close();
+	            }
+	        }
+	        catch (Exception ex) {
+	            ex.printStackTrace();
+	        }
+	        finally {
+	            if (con != null) con.close();
+	        }		
+		 
 	 }
 }

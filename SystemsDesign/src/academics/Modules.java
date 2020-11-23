8c9c8152ecd72b52f0ac46ae921f325bc77c3be5
FluffyCherub <73240956+FluffyCherub@users.noreply.github.com>
2020-11-23 01:06:39 +0000

    Start of the modules class
    nothing of any real substance here

---
+++ SystemsDesign/src/academics/Modules.java
@@ -0,0 +1,19 @@
+
+/*
+[-] Get module ID
+[-] Teachers module link?
+
+*/
+
+package academics;
+
+import java.sql.SQLException;
+
+public class Modules{
+    Integer moduleID;
+    String moduleName;
+    
+    public Modules() throws SQLException {
+    
+    }
+}

666619ca633a406c5307cea5fa82bf75efc9c5a5
Sangamithra Bala Amuthan <sbalaamuthan1@sheffield.ac.uk>
2020-12-01 23:29:20 +0000

    ahhhhh

--- SystemsDesign/src/view/Panels/Admin/addDeptPanel.java
+++ SystemsDesign/src/view/Panels/Admin/addDeptPanel.java
@@ -1,80 +1,68 @@
-package view.Panels.Admin;
+package view.Panels;
 
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 import userclasses.Admins;
 import view.Frames.AdminFrame;
 import view.Frames.LoginFrame;
-import view.Panels.Registrar.RegistrarMenu;
 
 import java.awt.EventQueue;
-import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JFrame;
 
 public class AddDeptPanel extends JPanel {
 	private JTextField idText;
 	private JTextField nameText;
 	private JTextField entryLevelText;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
 
 	/**
 	 * Create the panel.
 	 */
 	
 	
-	public AddDeptPanel(JPanel panel, JFrame mainFrame) {
-		panel.removeAll();
-		panel.revalidate();
-		panel.repaint();
+	public AddDeptPanel(JPanel contentPane) {
 		
 		setLayout(null);
-		setBounds(100, 100, 1035, 647);
 		
 		JLabel heading = new JLabel("New Department");
-		heading.setBounds(48, 37, 300, 20);
-		heading.setFont(new Font("Tahoma", Font.BOLD, 21));
-		panel.add(heading);
-		
-		System.out.println("Should be here..");
-
+		heading.setBounds(48, 37, 137, 20);
+		add(heading);
 		
 		JLabel deptId = new JLabel("Department ID");
 		deptId.setBounds(48, 93, 131, 20);
-		panel.add(deptId);
+		add(deptId);
 		
 		
 		idText = new JTextField();
 		idText.setBounds(187, 90, 146, 26);
-		panel.add(idText);
+		add(idText);
 		idText.setColumns(10);
 		
 		JLabel deptName = new JLabel("Department Name");
 		deptName.setBounds(48, 157, 137, 20);
-		panel.add(deptName);
+		add(deptName);
 		
 		nameText = new JTextField();
 		nameText.setBounds(187, 154, 146, 26);
-		panel.add(nameText);
+		add(nameText);
 		nameText.setColumns(10);
 		
 		JLabel entryLevel = new JLabel("Entry Level");
 		entryLevel.setBounds(48, 223, 108, 20);
-		panel.add(entryLevel);
+		add(entryLevel);
 		
 		entryLevelText = new JTextField();
 		entryLevelText.setBounds(187, 220, 146, 26);
-		panel.add(entryLevelText);
+		add(entryLevelText);
 		entryLevelText.setColumns(10);
 		
+		JButton cancelButton = new JButton("Cancel");
+		cancelButton.setBounds(48, 332, 115, 29);
+		add(cancelButton);
 		
 		JButton addButton = new JButton("Add");
 		addButton.setBounds(482, 332, 115, 29);
@@ -85,9 +73,8 @@ public class AddDeptPanel extends JPanel {
 					public void run() {
 						try {
 							Admins.addDepartment(idText.getText(), nameText.getText(), entryLevelText.getText());
-							JOptionPane.showMessageDialog(mainFrame.getComponent(0), "Department Added.");
-							JPanel menu = new AdminMenu(panel, mainFrame);
-							panel.add(menu);
+							AdminFrame frame = new AdminFrame();
+							frame.setVisible(true);
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
@@ -96,26 +83,7 @@ public class AddDeptPanel extends JPanel {
 	    	
 			}
 		});
-		panel.add(addButton);
-		
-		JButton cancelButton = new JButton("Cancel");
-		cancelButton.setBounds(48, 332, 115, 29);
-		panel.add(cancelButton);
-		cancelButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				setVisible(false);
-	    		EventQueue.invokeLater(new Runnable() {
-					public void run() {
-						try {
-							JPanel menu = new AdminMenu(panel, mainFrame);
-							panel.add(menu);
-						} catch (Exception e) {
-							e.printStackTrace();
-						}
-					}
-				});
-			}
-		});
-		
+		add(addButton);
+
 	}
 }
